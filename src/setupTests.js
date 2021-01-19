b751586f31c0d4e5c80aecc276e2763123da06b7
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-19 17:07:00 +0000

    add folders structures and setup files

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+//configure enzyme for all tests
+import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ 
+    adapter: new EnzymeAdapter(),
+    disableLifecycleMethods: true
+});
