5e2ac8e83b35c0d3be617973f406299e37b12a20
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-19 18:01:19 +0000

    print numbers on screen

--- src/components/GridContainer/GridContainer.js
+++ src/components/GridContainer/GridContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GridItem from '../GridItem/GridItem';
 
 export default class GridContainer extends React.Component {
     constructor() {
@@ -11,6 +12,23 @@ export default class GridContainer extends React.Component {
         };
     }
 
+    /**
+    * @function printNumbers creates array of 144 objects made up of number and active state
+	*/
+    printNumbers = () => {
+        const copy = [];
+        
+        for (let i = 1; i <= 144; i++) {
+            copy.push({ num: i, active: false });
+        }
+    
+        this.setState({ numbers: copy });
+    };
+
+    componentDidMount() {
+        this.printNumbers();
+    }
+
     render() {
         return (
             <div className="app-container">
@@ -19,7 +37,14 @@ export default class GridContainer extends React.Component {
                 <p className="app__description">This app displays 144 boxes on the screen. Click a box to view all the multiples of that number.</p>
 
                 <ul className="app-grid__container">
-
+                    {this.state.numbers.map((item) => (
+                        <GridItem 
+                            key={item.num}
+                            onClick={this.handleClick}
+                            className={item.active}
+                            number={item.num}
+                        />
+                    ))}
                 </ul>
             </div>
         )
--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function GridItem(props) {
+    const { active, number } = props;
+    
     return (
-        <li className="" >
-            test
+        <li className={`app-grid__item ${active ? "highlighted" : ""}`} >
+            {number}
         </li>
     )
 }
