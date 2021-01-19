3c790965f07e6d23fbcc8129bc86b4e78beb63a5
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-19 17:18:31 +0000

    change render test to enzyme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Grid from './components/GridContainer/GridContainer';
+
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			
+			<Grid />
 		</div>
 	);
 }
---
+++ src/components/GridContainer/GridContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class GridContainer extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="app-container">
+                
+            </div>
+        )
+    }
+}
